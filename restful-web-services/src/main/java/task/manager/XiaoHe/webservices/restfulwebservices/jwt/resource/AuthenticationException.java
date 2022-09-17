c469186e2c8ae13ff621792a26f36850961bac91
AlexHeeee <1085250220alex@gmail.com>
2022-09-17 15:04:32 -0700

    Initial commit

---
+++ restful-web-services/src/main/java/task/manager/XiaoHe/webservices/restfulwebservices/jwt/resource/AuthenticationException.java
@@ -0,0 +1,7 @@
+package task.manager.XiaoHe.webservices.restfulwebservices.jwt.resource;
+public class AuthenticationException extends RuntimeException {
+    public AuthenticationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
+
---
+++ restful-web-services/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into todo(id, username,description,target_date,is_done)
+values(10001, 'XiaoHe', 'Learn JPA', sysdate(), false);
+
+insert into todo(id, username,description,target_date,is_done)
+values(10002, 'XiaoHe', 'Learn Data JPA', sysdate(), false);
+
+insert into todo(id, username,description,target_date,is_done)
+values(10003, 'XiaoHe', 'Learn Microservices', sysdate(), false);
